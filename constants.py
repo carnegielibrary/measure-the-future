5a9c3d6b7af4eccbbfb70b3180a29729d146025b
Merritt Kowaleski <32393513+merrittkowaleski@users.noreply.github.com>
2017-12-01 15:25:53 -0500

    Several improvements
    - Add functionality to combine data from multiple datasets for the same scout
    - Add list of scouts to visualize to constants.py
    - Make smarter MTF_PATH constant that autodetects current folder and chooses a subfolder of it called mtf_data

--- constants.py
+++ constants.py
@@ -1,15 +1,27 @@
 from datetime import date
+import os
 
 #Set the folder containing all the MtF data you want to visualize:
 #(It is assumed that all subfolders of this folder are data for one scout)
-MTF_PATH = None
+#currently set to a subfolder of the folder this file is in called "mtf_data"
+MTF_PATH = os.path.dirname(os.path.realpath(__file__)) + os.sep + "mtf_data"
+
 
 #set the names of mtf_visualize notebooks to use
-MTF_VISUALIZE_NAMES = ['mtf_visualize_7_time', 'mtf_visualize_5_heatmap', 'mtf_visualize_5_paths']
+MTF_VISUALIZE_NAMES = ['mtf_visualize_8_time', 'mtf_visualize_5_heatmap', 'mtf_visualize_5_paths']
+
+#set the names of the scout folders you want to visualize
+SCOUT_FOLDER_NAMES = ['DB1 - Entry', 'DB2 - Childrens']
 
 # for mtf_visualize_5_paths
 NUM_PATHS_TO_DISPLAY = 150
 
-#for mtf_visualize_7_time
-WEEKDAYS_TO_DISPLAY = [5, 6] #Monday is 0, Sunday is 6
-DATE_RANGE = (date.min, date.max)
+#for mtf_visualize_8_time
+WEEKDAYS_TO_DISPLAY = list(range(7)) #Monday is 0, Sunday is 6
+
+#for mtf_visualize_5_paths, mtf_visualize_5_heatmap, mtf_visualize_8_time
+DATE_RANGE = (date(2017, 10, 25), date(2017, 11, 21))
+
+LOCALTZ = 'US/Eastern'
+
+MTF_JSON_ENCODING = 'ANSI'
--- load_mtf_data.py
+++ load_mtf_data.py
@@ -2,23 +2,32 @@
 import os
 import json
 from PIL import Image
-from datetime import datetime
 
-def load_interaction_data(scout_subfolder_path):    
-    interactions_path = scout_subfolder_path + os.sep + "scout_interactions.json"
+from datetime import date, datetime
+import pytz #time zone info
+
+from constants import LOCALTZ, MTF_JSON_ENCODING
+
+#----------
+#private
+
+def interaction_data(scout_subfolder_path):    
     interactions = None
     try:
+        interactions_path = scout_subfolder_path + os.sep + "scout_interactions.json"
         assert(os.path.isfile(interactions_path))
-        with open(interactions_path, 'r') as interactions_file:
+        with open(interactions_path, 'r', encoding=MTF_JSON_ENCODING) as interactions_file:
             interactions = json.load(interactions_file)
-    except Exception:
-        print("Problem loading interaction data in " + scout_subfolder_path + "\n")
+    except Exception as e:
+        print("Problem loading interaction data in " + scout_subfolder_path + "\n" + str(e))
+        interactions = None
     finally:
         return interactions
     
-def load_calibration_image(scout_subfolder_path, grayscale=True):
+def calibration_image(scout_subfolder_path, grayscale=True):
     im = None
     try:
+        image_path = None
         for filename in os.listdir(scout_subfolder_path):
             if filename.endswith("jpg") and filename.startswith("scout"):
                 image_path = scout_subfolder_path + os.sep + filename
@@ -28,38 +37,118 @@ def load_calibration_image(scout_subfolder_path, grayscale=True):
         print("Problem loading calibration image in " + scout_subfolder_path + "\n" + str(e))
     finally:
         return im
-    
-def combine_interaction_data_from_subfolders(scout_folder_path):
-    interactions = []
-    for filename in os.listdir(scout_folder_path):
-        scout_subfolder_path = scout_folder_path + os.sep + filename
-        if os.path.isdir(scout_subfolder_path):
-            next_interaction_set = load_interaction_data(scout_subfolder_path)
-            if next_interaction_set != None: interactions = interactions + next_interaction_set
-    return interactions
-
-def sort_key(folder_name):
-    #folder should be of the form scoutname_date
-    #where date is MMDDYYYY
+ 
+def parsed_set_date(set_subfolder_name):
+    #set_subfolder_name should be of the form "[scoutname]_[MMDDYYYY]"
     try:
         datestr = folder_name.split("_")[1]
-        date = datetime.strptime(datestr, "%m%d%Y")
+        date = datetime.strptime(datestr, "%m%d%Y").date()
     except:
         print("problem parsing date string in " + folder_name)
-        date = datetime(10, 1, 1)
+        date = date(10, 1, 1)
     finally:
         return date
+        
+def parsed_interaction_time(interaction_time_string):
+
+    #parse the time string into a python datetime object
+    dt = datetime.strptime(interaction_time_string, "%Y-%m-%dT%H:%M:%SZ")
+    #mtf records timestamps in UTC, so we need to tell the datetime object to interpret the parsed dates at UTC
+    dt = pytz.utc.localize(dt)
+    #tell the datetime object to change time display to eastern
+    dt = dt.astimezone(pytz.timezone(LOCALTZ))
     
-#returns tuple of (interactions, calibration image, subfolder name)
-def from_most_recent_subfolder(scout_folder_path):
+    return dt
     
-    #iterated through subfolders, sorted by parsed date from most recent to oldest
-    for filename in sorted(os.listdir(scout_folder_path), key=sort_key, reverse=True):
-        scout_subfolder_path = scout_folder_path + os.sep + filename
-        if os.path.isdir(scout_subfolder_path):
-            interactions = load_interaction_data(scout_subfolder_path)
-            im = load_calibration_image(scout_subfolder_path)
-            if interactions != None and im != None:
-                return (interactions, im, filename)
-    print("No usable subfolders in " + scout_folder_path + "\n")
-    return (None, None)
+def parse_interaction_times(interactions):
+    #replace string in "EnteredAt" field with a parsed datetime object in the local timezone.
+    for i in interactions: i['EnteredAt'] = parsed_interaction_time(i['EnteredAt'])
+    return interactions
+    
+# from https://stackoverflow.com/questions/9427163/remove-duplicate-dict-in-list-in-python
+def deduplicate(list_of_jsons):
+    #return [elem for n, elem in enumerate(list) if elem not in list[n + 1:]] 
+    #return [dict(t) for t in set([tuple(d.items()) for d in list])]
+    set_of_jsons = {json.dumps(d, sort_keys=True) for d in list_of_jsons}
+    return [json.loads(t) for t in set_of_jsons]
+ 
+#-------------------
+#public
+ 
+#returns tuple of (interactions, calibration image) 
+def from_set(scout_folder_path, set_subfolder_name):
+    path = scout_folder_path + os.sep + set_subfolder_name
+    if os.path.isdir(path): return (interaction_data(path), calibration_image(path))
+    else: return (None, None)
+ 
+#returns tuple of (interactions, calibration image, set subfolder name)
+def from_most_recent_set(scout_folder_path):
+    
+    #iterate through subfolders, sorted by parsed date from most recent to oldest
+    for set_subfolder_name in sorted(os.listdir(scout_folder_path), key=parsed_set_date, reverse=True):
+        (interactions, im) = from_set(scout_folder_path, set_subfolder_name)
+        if interactions != None and im != None:
+            return (interactions, im, set_subfolder_name)
+    print("No usable set subfolders in " + scout_folder_path + "\n")
+    return (None, None, None) 
+ 
+#the calibration image returned from the following methods is a bit tricky.=
+#combining data with different calibration images can be dubious if the images
+#are angled differently to a signifigant degree (which is very possible).
+#so using the following options assumes that either:
+#   a) the calibration image won't be used (such as in the case of time charts)
+#   b) the user has already looked through the images manually and confirmed
+#       that they are all sufficiently similar in angle for the data to
+#       match up reasonably. 
+#
+#additionally, in the case of b) the user should select the "best" calibration
+#image (the sharpest and most free of people and temoprary objects and 
+#put a copy of it in the scout's folder (the main folder, not a set subfolder).
+#if an image exists there, these functions will return it as the overall calibration
+#image to display as background. if not, an arbitrary one will be selected.
+ 
+#returns tuple of (interactions, calibration image)    
+def combined_from_sets(scout_folder_path, set_subfolder_names):
+    im_overall = None
+    im_overall = calibration_image(scout_folder_path)
+    
+    interactions_all = []
+    for set_subfolder_name in set_subfolder_names:
+        (interactions, im) = from_set(scout_folder_path, set_subfolder_name)
+        if interactions != None: interactions_all = interactions_all + interactions
+        if im_overall == None: im_overall = im
+        
+    return (parse_interaction_times(deduplicate(interactions_all)), im_overall) 
+    #dedupe interactions_all in case data was not cleared between collecting datasets
+ 
+#returns tuple of (interactions, calibration image)    
+def combined_from_all_sets(scout_folder_path):
+    return combined_from_sets(scout_folder_path, os.listdir(scout_folder_path))
+    
+#returns tuple of (interactions, calibration image)    
+#min_date and max_date should be datetime objects
+#weeksdays to include should be a list of numbers between 1 and 7 
+def from_date_range(scout_folder_path, date_range, weekdays_to_include=range(7)):
+    (interactions, im) = combined_from_all_sets(scout_folder_path)
+    
+    clipped_interactions = []
+    for i in interactions:
+        dt = i['EnteredAt']
+        if dt.weekday() in weekdays_to_include and date_range[0] <= dt.date() <= date_range[1]:
+            clipped_interactions.append(i)
+            
+    return (clipped_interactions, im)
+    
+    '''
+    relevant_sets = []
+    #iterate through subfolders, sorted by parsed date from oldest to most recent
+    for set_subfolder_name in sorted(os.listdir(scout_folder_path), key=parsed_set_date):
+        if parsed_set_date(set_subfolder_name)
+        
+        (interactions, im) = from_set(scout_folder_path, set_subfolder_name)
+        if interactions != None and im != None:
+            return (interactions, im, set_subfolder_name)
+    print("No usable set subfolders in " + scout_folder_path + "\n")
+    return (None, None, None) 
+    '''
+            
